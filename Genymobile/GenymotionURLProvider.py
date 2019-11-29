c93e6cfaa66317b412dc4f1bf7bae04850fffa3b
Elliot Jordan <elliot@elliotjordan.com>
2019-11-29 15:13:59 -0800

    Update GenymotionURLProvider to use URLGetter superclass

--- Genymobile/GenymotionURLProvider.py
+++ Genymobile/GenymotionURLProvider.py
@@ -1,45 +1,44 @@
 #!/usr/bin/python
 
-from __future__ import absolute_import
-
 import json
-import urllib2
 
-from autopkglib import Processor, ProcessorError
+from autopkglib import Processor, ProcessorError, URLGetter
 
 __all__ = ["GenymotionURLProvider"]
 
-class GenymotionURLProvider(Processor):
-    decription = ("Uses Genymotion API to find the latest version of application.")
-    input_variables = {
-    }
-    output_variables = {
-        'genymotion_version': {
-            'description': 'Version number',
-        }
-    }
+
+class GenymotionURLProvider(URLGetter):
+    """Processor class."""
+
+    decription = "Uses Genymotion API to find the latest version of application."
+    input_variables = {}
+    output_variables = {"genymotion_version": {"description": "Version number",}}
 
     description = __doc__
 
     def get_version(self):
+        """Get version from Genymotion API."""
+
         # Genymotion Update API
-        url = 'https://cloud.genymotion.com/launchpad/last_version/mac/x64/'
+        url = "https://cloud.genymotion.com/launchpad/last_version/mac/x64/"
         # API requires AJAX / XMLHttpRequest
-        headers = {'X-Requested-With': 'XMLHttpRequest', 'User-Agent': 'Safari'}
+        headers = {"X-Requested-With": "XMLHttpRequest", "User-Agent": "Safari"}
         try:
-            request = urllib2.Request(url, headers=headers)
-            response = urllib2.urlopen(request)
+            response = self.download(url, headers=headers)
             # Convert response to JSON
-            jsondata = json.loads(response.read())
-            # Example json return - {u'url': u'http://files2.genymotion.com/genymotion/genymotion-2.7.2/genymotion-2.7.2.dmg', u'version': u'2.7.2'}
-            return jsondata['version']
+            jsondata = json.loads(response)
+            # Example json return: {u'url': u'http://files2.genymotion.com/genymotion/genymotion-2.7.2/genymotion-2.7.2.dmg', u'version': u'2.7.2'}
+            return jsondata["version"]
         except:
-            raise ProcessorError('Could not retrieve version from URL: %s' % url)
+            raise ProcessorError("Could not retrieve version from URL: %s" % url)
 
     def main(self):
-        self.env['genymotion_version'] = self.get_version()
-        self.output('Version: %s' % self.env['genymotion_version'])
+        """Main process."""
 
-if __name__ == '__main__':
-    processor = GenymotionURLProvider()
-    processor.execute_shell()
+        self.env["genymotion_version"] = self.get_version()
+        self.output("Version: %s" % self.env["genymotion_version"])
+
+
+if __name__ == "__main__":
+    PROCESSOR = GenymotionURLProvider()
+    PROCESSOR.execute_shell()
