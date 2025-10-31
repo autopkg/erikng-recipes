d3e9b21263e2b62f74a1f4930258bd928b5cccd3
Erik Gomez <egomez@coreweave.com>
2025-10-31 10:35:39 -0500

    try a processor

---
+++ PathDeleter/PathDeleter.py
@@ -0,0 +1,28 @@
+from autopkglib import Processor, ProcessorError
+import os
+import shutil
+
+__all__ = ["PathDeleter"]
+
+class PathDeleter(Processor):
+    description = "Deletes files/folders from a list."
+    input_variables = {
+        "path_list": {
+            "required": True,
+            "description": "List of paths to delete"
+        }
+    }
+
+    def main(self):
+        for path in self.env.get("path_list", []):
+            if os.path.exists(path):
+                print(f"Deleting {path}")
+                try:
+                    if os.path.isdir(path):
+                        shutil.rmtree(path)
+                    else:
+                        os.remove(path)
+                except Exception as e:
+                    raise ProcessorError(f"Failed to delete {path}: {e}")
+            else:
+                print(f"Path does not exist: {path}")
